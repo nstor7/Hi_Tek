8fd8528a0487f67269caea886a28ce57697d464b
nstor7 <nstor7@hotmail.com>
2015-06-25 17:31:25 -0500

    boto de nav

--- menu.js
+++ menu.js
@@ -1,6 +1,21 @@
-var boton = document.getElementById("menu");
-var barra = document.getElementById("nav");
-Function navegacion(){
-    barra.style.display = "inline";
-}
-document.addEventListener("Click", "#menu", true);
+ var menu=document.getElementById("menu");
+        var nav=document.getElementById("nav");
+        var i = 0;
+        var navDespliegue = function()
+        {
+            
+            if(i===0)
+            {
+                
+                nav.style.display = "block";
+                i = 1;
+            }
+            else{
+              nav.style.display = "none"; 
+              i = 0; 
+            };
+            
+            
+        };
+        
+        menu.addEventListener("click", navDespliegue);
--- min/menu-min.js
+++ min/menu-min.js
@@ -1,1 +1,1 @@
-function inicio(){var i=document.getElementById("nav");estilo=i.style,console.log(estilo)}inicio();
+var menu=document.getElementById("menu"),nav=document.getElementById("nav"),i=0,navDespliegue=function(){0===i?(nav.style.display="block",i=1):(nav.style.display="none",i=0)};menu.addEventListener("click",navDespliegue);
