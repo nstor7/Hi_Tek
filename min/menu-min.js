2089368c516e884bf725ffdc391e62ec5950bd40
Néstor Ibarra <nstor7@hotmail.com>
2015-08-25 11:27:40 -0500

    Reorganización de carpetas y creación del servidor local

--- min/menu-min.js
+++
@@ -1,1 +0,0 @@
-var menu=document.getElementById("menu"),nav=document.getElementById("nav"),i=0,navDespliegue=function(){0===i?(nav.style.display="block",i=1):(nav.style.display="none",i=0)};menu.addEventListener("click",navDespliegue);
