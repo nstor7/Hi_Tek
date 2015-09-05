613d95b779a71bceb6655e97e9bfce90184b9335
Néstor Ibarra <nstor7@hotmail.com>
2015-09-05 11:50:06 -0500

    ip

--- servidor.js
+++ servidor.js
@@ -5,7 +5,7 @@ var parcero = require('body-parser');
 app.use(parcero.urlencoded({extended:true}));
 var servidor;
 
-servidor = app.listen(80, 'ip', function(){
+servidor = app.listen(80, '172.31.38.114', function(){
     console.log('Servidor Arrancado');
 });
 
