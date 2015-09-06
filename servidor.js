519e2f06e985c9a9a59cd9fc1e1694ab9a1425f2
Néstor Ibarra <nstor7@hotmail.com>
2015-09-06 10:06:13 -0500

    http corrección 2

--- servidor.js
+++ servidor.js
@@ -5,6 +5,11 @@ var parcero = require('body-parser');
 app.use(parcero.urlencoded({extended:true}));
 var http = require('http');
 
-http.createServer(app).listen(80)
+http.createServer(app).listen(80);
+app.listen = function(){
+    var servidor = http.createServer(this);
+    return server.listen.apply(server, arguments)
+    console.log('exitoso puerto http')
+};
 
 app.use(express.static('pagina'));
